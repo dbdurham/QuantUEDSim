9c500776c19f6cb5c498308d62b1876b6ce6346d
Dan Durham <dbdurham@berkeley.edu>
2022-05-30 17:01:51 -0500

    computeRStack: Update R factor calculation to include scaling factor correction

--- Utilities/computeRStack.m
+++ Utilities/computeRStack.m
@@ -1,11 +1,24 @@
-function Rend = computeRStack(IArray)
+function R = computeRStack(IArray)
 %COMPUTERSTACK Compute R for a stack of diffracted intensities referenced
 %to the end of the stack
 %   IArray - MxNxP array of peak intensities (order,thickness,parameter)
 
-Rend = 100*squeeze(...
-    sum(abs(IArray(:,:,1:end) - IArray(:,:,end)),1) ...
-    ./ sum(IArray(:,:,end),1));
+Np = size(IArray,3);
+Nt = size(IArray,2);
+R = zeros(Nt,Np);
+% alphaStore = zeros(Nt,Np);
 
+for ip = 1:Np-1
+    for it = 1:Nt
+        I = IArray(:,it,ip);
+        Iref = IArray(:,it,end);
+        % Correct for scaling
+        alpha = I\Iref;
+%         alphaStore(it,ip) = alpha;
+        I = I*alpha;
+        % Compute R
+        R(it,ip) = 100*sum(abs(I-Iref)) ./ sum(Iref);
+    end
 end
+
 
