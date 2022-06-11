439c29dee998c113bbe5929d0543ee85dcdbe20b
Dan Durham <dbdurham@berkeley.edu>
2022-06-11 16:12:41 -0500

    computeRStack: Use I^1/2 instead of I

--- Utilities/computeRStack.m
+++ Utilities/computeRStack.m
@@ -1,5 +1,5 @@
 function R = computeRStack(IArray)
-%COMPUTERSTACK Compute R for a stack of diffracted intensities referenced
+%COMPUTERSTACK Compute R (%) for a stack of diffracted intensities referenced
 %to the end of the stack
 %   IArray - MxNxP array of peak intensities (order,thickness,parameter)
 
@@ -8,15 +8,17 @@ Nt = size(IArray,2);
 R = zeros(Nt,Np);
 % alphaStore = zeros(Nt,Np);
 
+gam = 0.5;
+
 for ip = 1:Np-1
     for it = 1:Nt
-        I = IArray(:,it,ip);
-        Iref = IArray(:,it,end);
+        I = IArray(:,it,ip).^gam;
+        Iref = IArray(:,it,end).^gam;
         % Correct for scaling
         alpha = I\Iref;
 %         alphaStore(it,ip) = alpha;
         I = I*alpha;
-        % Compute R
+        % Compute R (%)
         R(it,ip) = 100*sum(abs(I-Iref)) ./ sum(Iref);
     end
 end
