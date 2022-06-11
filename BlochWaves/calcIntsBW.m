d2dab5e7afaffa9b58345a589425a8f125231abd
Dan Durham <dbdurham@berkeley.edu>
2022-06-11 13:06:24 -0500

    calcIntsBW: Revert unpacking of sDiff struct

--- BlochWaves/calcIntsBW.m
+++ BlochWaves/calcIntsBW.m
@@ -7,29 +7,24 @@ function [Iarray,psi_G_array,hklSel,GhklSel] = calcIntsBW(theta1,theta2,...
 %   sDiff = setup struct
 %   NOTE: ASSUMES CENTROSYMMETRIC CRYSTAL
 
-% Unpack input variable structure
-fieldNames = fieldnames(sDiff);
-nFields = numel(fieldNames);
-for iField = 1:nFields
-    [~] = evalc([fieldNames{iField} ' = sDiff.' fieldNames{iField}]);
-end
-
 % Compute excitation errors (inv Angstroms)
-s_G = computeExcitationError(theta1,theta2,Ghkl,lambElec); 
+
+s_G = computeExcitationError(theta1,theta2,sDiff.Ghkl,sDiff.lambElec); 
 
 % Select beams within specified reciprocal space volume around 
 % the Ewald sphere
-Gxy = sqrt(Ghkl(:,1).^2 + Ghkl(:,2).^2);
+Gxy = sqrt(sDiff.Ghkl(:,1).^2 + sDiff.Ghkl(:,2).^2);
 % Gz = abs(Ghkl(:,3));
-isSel = Gxy < GxyThresh ...
-    & ~(U_G==0) ...
-    & abs(s_G) < sThresh;
-hklSel = hkl(isSel,:);
-GhklSel = Ghkl(isSel,:);
+isSel = Gxy < sDiff.GxyThresh ...
+    & ~(sDiff.U_G==0) ...
+    & abs(s_G) < sDiff.sThresh;
+hklSel = sDiff.hkl(isSel,:);
+GhklSel = sDiff.Ghkl(isSel,:);
+
 
 %% Build and solve matrix equation
 N = sum(isSel);
-k0 = 1./lambElec;
+k0 = 1./sDiff.lambElec;
 k0z = k0*(-cos(theta1)*cos(theta2));
 
 hklDiff = zeros(N,N,3);
@@ -38,29 +33,35 @@ for ii = 1:3
         -repmat(hklSel(:,ii)',[N 1]);
 end
 
-if any(hklDiff(:,:,1) > hRange(2) | hklDiff(:,:,1) < hRange(1),'all')
+if any(hklDiff(:,:,1) > sDiff.hRange(2) ...
+        | hklDiff(:,:,1) < sDiff.hRange(1),'all')
     disp('h out of range, need more h points')
 end
-if any(hklDiff(:,:,2) > kRange(2) | hklDiff(:,:,2) < kRange(1),'all')
+if any(hklDiff(:,:,2) > sDiff.kRange(2) ...
+        | hklDiff(:,:,2) < sDiff.kRange(1),'all')
     disp('k out of range, need more k points')
 end
-if any(hklDiff(:,:,3) > lRange(2) | hklDiff(:,:,3) < lRange(1),'all')
+if any(hklDiff(:,:,3) > sDiff.lRange(2) ...
+        | hklDiff(:,:,3) < sDiff.lRange(1),'all')
     disp('l out of range, need more l points')
 end
 
-indDiff = sub2ind([hLen,kLen,lLen],...
-    hklDiff(:,:,1)-(hRange(1)-1),...
-    hklDiff(:,:,2)-(kRange(1)-1),...
-    hklDiff(:,:,3)-(lRange(1)-1));
 
-A = diag(2*k0.*s_G(isSel))+U_G(indDiff).*(ones(N,N)-diag(ones(N,1)));
+indDiff = sub2ind([sDiff.hLen,sDiff.kLen,sDiff.lLen],...
+    hklDiff(:,:,1)-(sDiff.hRange(1)-1),...
+    hklDiff(:,:,2)-(sDiff.kRange(1)-1),...
+    hklDiff(:,:,3)-(sDiff.lRange(1)-1));
+
+A = diag(2*k0.*s_G(isSel))+sDiff.U_G(indDiff).*(ones(N,N)-diag(ones(N,1)));
 
 [Cvecs,eigvals] = eig(A);
 Cvecsinv = conj(Cvecs');
+
 % Cvecsinv = inv(Cvecs);
 
 %% Compute exit wave components
 % Initial condition (plane wave)
+
 psiInit_G = zeros(N,1);
 psiInit_G(hklSel(:,1)==0 & hklSel(:,2)==0 & hklSel(:,3)==0) = 1;
 
@@ -68,7 +69,7 @@ gam = eigvals/(2*k0z); % Inverse Angstroms
 
 psi_G = @(z) Cvecs*(eye(N).*exp(2i*pi*gam*z))*Cvecsinv*psiInit_G;
 
-dz = cellDim(3);
+dz = sDiff.cellDim(3);
 zTest = (1:nUC)*dz;
 nZ = numel(zTest);
 
