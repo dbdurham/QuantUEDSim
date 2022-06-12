07482f88425d14d09a2df969dbac2c2fecd84808
Dan Durham <dbdurham@berkeley.edu>
2022-06-12 17:44:59 -0500

    computeWeightedDiffStack: speedup matrix multiplication to 4D array

--- Utilities/computeWeightedDiffStack.m
+++ Utilities/computeWeightedDiffStack.m
@@ -2,11 +2,11 @@ function weightedDiffStack = computeWeightedDiffStack(theta1,theta2,...
     sigmaThetaSamp,symmDPs,funcDiff)
 %COMPUTEWEIGHTEDDIFFSTACK Summary of this function goes here
 %   Detailed explanation goes here
-nTheta = numel(sigmaThetaSamp);
-diffMat = repmat(funcDiff(theta1,theta2),[1 1 1 nTheta]);
-diffSize = size(diffMat);
-weightMat = repmat(permute(computeGaussianTiltSpreadWeights(theta1,theta2,...
-            sigmaThetaSamp,symmDPs),[3 4 1 2]),[diffSize(1:2) 1]);
+
+diffMat = funcDiff(theta1,theta2);
+
+weightMat = permute(computeGaussianTiltSpreadWeights(theta1,theta2,...
+            sigmaThetaSamp,symmDPs),[3 4 1 2]);
 weightedDiffStack = weightMat.*diffMat;
 
 end
