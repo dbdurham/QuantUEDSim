b711bb39f3e2c22209a8404a9d6ed04cc295034d
Dan Durham <dbdurham@berkeley.edu>
2022-06-12 13:30:48 -0500

    calcDiffKin: Add downsampling for easier storage

--- Kinematical/calcDiffKin.m
+++ Kinematical/calcDiffKin.m
@@ -1,9 +1,11 @@
-function IDiff = calcDiffKin(theta1,theta2,nUC,sDiff)
+function IDiffStore = calcDiffKin(theta1,theta2,nUC,sDiff)
 %CALCDIFFBW Calculate diffraction pattern stack using Kinematical method
 %   Detailed explanation goes here
 
 IArray = calcIntsKin(theta1,theta2,nUC,sDiff);
 IDiff = projectIntsToDP(IArray,sDiff.Ghkl,sDiff.qxa,sDiff.qya);
+% Downsample for storage
+IDiffStore = IDiff(1:sDiff.downSampFac:end,1:sDiff.downSampFac:end,:);
 
 end
 
