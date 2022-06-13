8fd060b3c074ab7e9c3a878f38b5434e90b656b1
Dan Durham <dbdurham@berkeley.edu>
2022-06-12 23:09:40 -0500

    calcIntsBW: Matrix operation speedups

--- BlochWaves/calcIntsBW.m
+++ BlochWaves/calcIntsBW.m
@@ -8,7 +8,6 @@ function [Iarray,psi_G_array,hklSel,GhklSel] = calcIntsBW(theta1,theta2,...
 %   NOTE: ASSUMES CENTROSYMMETRIC CRYSTAL
 
 % Compute excitation errors (inv Angstroms)
-
 s_G = computeExcitationError(theta1,theta2,sDiff.Ghkl,sDiff.lambElec); 
 
 % Select beams within specified reciprocal space volume around 
@@ -23,6 +22,7 @@ GhklSel = sDiff.Ghkl(isSel,:);
 
 
 %% Build and solve matrix equation
+
 N = sum(isSel);
 k0 = 1./sDiff.lambElec;
 k0z = k0*(-cos(theta1)*cos(theta2));
@@ -57,6 +57,7 @@ A = diag(2*k0.*s_G(isSel))+sDiff.U_G(indDiff).*(ones(N,N)-diag(ones(N,1)));
 [Cvecs,eigvals] = eig(A);
 Cvecsinv = conj(Cvecs');
 
+
 % Cvecsinv = inv(Cvecs);
 
 %% Compute exit wave components
@@ -65,9 +66,11 @@ Cvecsinv = conj(Cvecs');
 psiInit_G = zeros(N,1);
 psiInit_G(hklSel(:,1)==0 & hklSel(:,2)==0 & hklSel(:,3)==0) = 1;
 
-gam = eigvals/(2*k0z); % Inverse Angstroms
+gam = diag(eigvals)/(2*k0z); % Inverse Angstroms
 
-psi_G = @(z) Cvecs*(eye(N).*exp(2i*pi*gam*z))*Cvecsinv*psiInit_G;
+RHS = Cvecsinv*psiInit_G;
+
+psi_G = @(z) Cvecs*(exp(2i*pi*gam*z).*RHS);
 
 dz = sDiff.cellDim(3);
 zTest = (1:nUC)*dz;
